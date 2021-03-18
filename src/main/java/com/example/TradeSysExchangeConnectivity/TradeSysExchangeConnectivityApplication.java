72f4e32a8d20669520bd6ba3ea1726fe25e7e42f
nunyaklah <nunyaklah@gmail.com>
2021-03-18 17:32:44 +0000

    testing api

--- src/main/java/com/example/TradeSysExchangeConnectivity/TradeSysExchangeConnectivityApplication.java
+++ src/main/java/com/example/TradeSysExchangeConnectivity/TradeSysExchangeConnectivityApplication.java
@@ -22,5 +22,5 @@ public class TradeSysExchangeConnectivityApplication {
 		return List.of("Hello" , "Exchange");
 	}
 
-	////////////////////////////////
+	/////////////////////////////////////////
 }
