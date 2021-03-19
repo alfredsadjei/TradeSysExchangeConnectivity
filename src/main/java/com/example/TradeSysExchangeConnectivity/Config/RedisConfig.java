01cfdd4e1948fff6dafe969ea88d79664797acb0
nunyaklah <nunyaklah@gmail.com>
2021-03-19 16:07:37 +0000

    created redis client for exchange

---
+++ src/main/java/com/example/TradeSysExchangeConnectivity/Config/RedisConfig.java
@@ -0,0 +1,36 @@
+package com.example.TradeSysExchangeConnectivity.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory(){
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName("redis-17849.c59.eu-west-1-2.ec2.cloud.redislabs.com");
+        redisStandaloneConfiguration.setPort(17849);
+        redisStandaloneConfiguration.setPassword("EWbpysz7KwsBbFo4xXBvQ5qx2IwWKlOH");
+
+        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory((redisStandaloneConfiguration));
+        return  jedisConnectionFactory;
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(){
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(jedisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new JdkSerializationRedisSerializer());
+        redisTemplate.setEnableTransactionSupport(true);
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+}
--- src/main/java/com/example/TradeSysExchangeConnectivity/Exchange/MakeOrder.java
+++ src/main/java/com/example/TradeSysExchangeConnectivity/Exchange/MakeOrder.java
@@ -1,4 +1,11 @@
 package com.example.TradeSysExchangeConnectivity.Exchange;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Collections;
+
 public class MakeOrder {
+
 }
